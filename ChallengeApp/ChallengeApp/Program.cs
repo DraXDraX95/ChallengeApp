dd770d40e33c213e82a0a4f96129a0150ee4e61d
Patryk Romanowski <patryk95513@gmail.com>
2023-05-17 18:35:18 +0200

    Praca Domowa dzień 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+
+
+var name = "Ewa";
+int age = 29;
+var gender = "Kobieta";
+int age2 = 33;
+if (age < 46)
+{
+    Console.WriteLine("Kobieta, poniżej 30 lat");
+} 
+else
+{
+   Console.WriteLine("Kobieta, powyżej 30 lat");
+}
+ if (name == "Ewa" && age2 == 33)
+{
+    Console.WriteLine("Ewa, lat 33");
+
+}
+else if (gender == "Mężczyzna" && age > 15) 
+{
+    Console.WriteLine("Niepełnoletni Mężczyzna");
+}
+
+
+
+
+
+
+
+
+
+
+
+
