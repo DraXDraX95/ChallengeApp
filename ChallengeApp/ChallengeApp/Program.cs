b26905fd197f44fec42cfe486656e286b97f25ed
Patryk Romanowski <patryk95513@gmail.com>
2024-04-02 16:59:12 +0200

    Praca domowa na dzień 6

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,67 +1,57 @@
-﻿var number = 7897;
- string numberinstring = number. ToString();
-char[] letters= numberinstring.ToArray();
-var counter0 = 0;
-var counter1 = 0;
-var counter2 = 0;
-var counter3 = 0;
-var counter4 = 0;
-var counter5 = 0;
-var counter6 = 0;
-var counter7 = 0;
-var counter8 = 0;
-var counter9 = 0;
-foreach (char letter in letters)
+﻿using ChallengeApp;
+
+
+Employee employee1 = new Employee(" Jarosław ", " Jareczek",45);
+Employee employee2 = new Employee("Marysia", "Ktosiowa", 23);
+Employee employee3 = new Employee("Dionizy", "Izydor", 63);
+
+employee1.AddScore(4);
+employee1.AddScore(8);
+employee1.AddScore(3);
+employee1.AddScore(9);
+employee1.AddScore(1);
+
+employee2.AddScore(1);
+employee2.AddScore(4);
+employee2.AddScore(9);
+employee2.AddScore(10);
+employee2.AddScore(4);
+
+employee3.AddScore(0);
+employee3.AddScore(9);
+employee3.AddScore(5);
+employee3.AddScore(10);
+employee3.AddScore(4);
+
+  var employyes = new List<Employee> { employee1, employee2, employee3 };
+
+int maxResult = -1;
+Employee employeeWhithmaxResult = null;
+
+ foreach (var employee in employyes)
 {
-    if (letter == '0')
+    if (employee.Result > maxResult)
     {
-        counter0++;
-    }
-    else if (letter == '1')
-    {
-        counter1++;
-    }
-    else if (letter == '2')
-    {
-        counter2++;
-    }
-    else if (letter == '3')
-    {
-        counter3++;
-    }
-    else if (letter == '4')
-    {
-        counter4++;
-    }
-    else if (letter == '5')
-    {
-        counter5++;
-    }
-    else if (letter == '6')
-    {
-        counter6++;
-    }
-    else if (letter == '7')
-    {
-        counter7++;
-    }
-    else if (letter == '8')
-    {
-        counter8++;
-    }
-    else if (letter == '9')
-    {
-        counter9++;
+        maxResult = employee.Result;
+        employeeWhithmaxResult = employee;
     }
     
 }
-Console.WriteLine("0=>" + counter0);
-Console.WriteLine("1=>" + counter1);
-Console.WriteLine("2=>" + counter2);
-Console.WriteLine("3=>" + counter3);
-Console.WriteLine("4=>" + counter4);
-Console.WriteLine("5=>" + counter5);
-Console.WriteLine("6=>" + counter6);
-Console.WriteLine("7=>" + counter7);
-Console.WriteLine("8=>" + counter8);
-Console.WriteLine("9=>" + counter9);
+
+Console.WriteLine("Osoba Wygrywająca Konkurs (Best Employye)");
+Console.WriteLine(employeeWhithmaxResult.Name);
+Console.WriteLine(employeeWhithmaxResult.Surname);
+Console.WriteLine(employeeWhithmaxResult.Age + "" + "lata");
+Console.WriteLine("Ilość zdobytych punktów");
+Console.WriteLine(maxResult);
+
+Console.ReadKey();
+
+
+
+
+
+
+
+
+
